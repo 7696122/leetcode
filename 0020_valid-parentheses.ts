7175bc79ebd3058d7446147f7094bc00c04e3d41
7696122 <7696122@gmail.com>
2021-10-05 06:55:40 +0900

    0020_valid-parentheses.ts

--- 0020_valid-parentheses.ts
+++ 0020_valid-parentheses.ts
@@ -1,5 +1,5 @@
 function isValid(s: string): boolean {
-  // let Invalid = new Error("Invalid");
+  let Invalid = new Error("Invalid");
   const stack = [];
 
   const BRACKETS = {
@@ -8,26 +8,18 @@ function isValid(s: string): boolean {
     "[": "]",
   };
 
-  s.split("").some((item) => {
-    if (BRACKETS[item]) {
-      stack.push(item);
-    } else {
-      return stack.length === 0 || BRACKETS[stack.pop()] !== item;
-    }
-  });
-
-  // try {
-  //   s.split("").forEach((item) => {
-  //     if (BRACKETS[item]) {
-  //       stack.push(item);
-  //     } else {
-  //       if (stack.length === 0 || BRACKETS[stack.pop()] !== item) {
-  //         throw Invalid;
-  //       }
-  //     }
-  //   });
-  //   return stack.length === 0;
-  // } catch (e) {
-  //   return false;
-  // }
+  try {
+    s.split("").forEach((item) => {
+      if (BRACKETS[item]) {
+        stack.push(item);
+      } else {
+        if (stack.length === 0 || BRACKETS[stack.pop()] !== item) {
+          throw Invalid;
+        }
+      }
+    });
+    return stack.length === 0;
+  } catch (e) {
+    return false;
+  }
 }
