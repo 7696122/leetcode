53ae185d2248b4e3bfff2c49302f37ceb988111c
7696122 <7696122@gmail.com>
2021-10-06 06:24:12 +0900

    0083_remove-duplicates-from-sorted-list.ts

--- 0083_remove-duplicates-from-sorted-list.ts
+++ 0083_remove-duplicates-from-sorted-list.ts
@@ -11,21 +11,22 @@
  */
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-  // let sentinel = head;
-  // while (sentinel !== null && sentinel.next !== null) {
-  //   if (sentinel.val === sentinel.next.val) {
-  //     sentinel.next = sentinel.next.next;
+  let sentinel = head;
+  while (sentinel !== null && sentinel.next !== null) {
+    if (sentinel.val === sentinel.next.val) {
+      sentinel.next = sentinel.next.next;
+    } else {
+      sentinel = sentinel.next;
+    }
+  }
+
+  // while (head !== null && head.next !== null) {
+  //   if (head.val === head.next.val) {
+  //     head.next = head.next.next;
   //   } else {
-  //     sentinel = sentinel.next;
+  //     head = head.next;
   //   }
   // }
 
-  while (head !== null && head.next !== null) {
-    if (head.val === head.next.val) {
-      head.next = head.next.next;
-    } else {
-      head = head.next;
-    }
-  }
   return head;
 }
