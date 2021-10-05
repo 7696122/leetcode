abcf52d47157fe966abc288a2fde8f35eaffd708
7696122 <7696122@gmail.com>
2021-10-06 05:53:32 +0900

    0083_remove-duplicates-from-sorted-list.ts

--- 0083_remove-duplicates-from-sorted-list.ts
+++ 0083_remove-duplicates-from-sorted-list.ts
@@ -11,7 +11,7 @@
  */
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-  if (head === null) return head;
+  if (head.next === null) return head;
   if (head.val === head.next.val) {
     head.next = head.next.next;
     return deleteDuplicates(head);
