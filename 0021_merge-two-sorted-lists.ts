63a9901820ed2c8dcdb1083dc037fd6b2bece673
7696122 <7696122@gmail.com>
2021-10-06 04:56:45 +0900

    0021_merge-two-sorted-lists.ts

--- 0021_merge-two-sorted-lists.ts
+++ 0021_merge-two-sorted-lists.ts
@@ -14,11 +14,13 @@ function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  const result: ListNode = null;
   if (l1 === null) return l2;
   if (l2 === null) return l1;
   if (l1.value < l2.value) {
     l1.next = mergeTwoLists(l1, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2, l1);
+    return l2;
   }
-  return result;
 }
