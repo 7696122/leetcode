36834a29bb16a418ccd8716b3b6ab64f5a043bbf
7696122 <7696122@gmail.com>
2021-10-06 05:37:20 +0900

    0021_merge-two-sorted-lists.ts

--- 0021_merge-two-sorted-lists.ts
+++ 0021_merge-two-sorted-lists.ts
@@ -14,16 +14,6 @@ function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  // if (l1 === null) return l2;
-  // if (l2 === null) return l1;
-  // if (l1.val < l2.val) {
-  //   l1.next = mergeTwoLists(l1.next, l2);
-  //   return l1;
-  // } else {
-  //   l2.next = mergeTwoLists(l2.next, l1);
-  //   return l2;
-  // }
-
   const sentinel: ListNode = new ListNode(-1, null);
 
   let head: ListNode = sentinel;
@@ -42,4 +32,14 @@ function mergeTwoLists(
   head.next = l1 || l2;
 
   return sentinel.next;
+
+  // if (l1 === null) return l2;
+  // if (l2 === null) return l1;
+  // if (l1.val < l2.val) {
+  //   l1.next = mergeTwoLists(l1.next, l2);
+  //   return l1;
+  // } else {
+  //   l2.next = mergeTwoLists(l2.next, l1);
+  //   return l2;
+  // }
 }
