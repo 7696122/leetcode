dd1333aaa15aed085c5769d63b9ecc503b8a8a96
7696122 <7696122@gmail.com>
2021-10-08 07:50:30 +0900

    0027_remove-element.ts

--- 0027_remove-element.ts
+++ 0027_remove-element.ts
@@ -1,7 +1,7 @@
 function removeElement(nums: number[], val: number): number {
   const filtered = nums.filter((num) => num !== val);
-  nums.forEach((num, i) => {
-    nums[i] = filtered[i] ? filtered[i] : null;
+  filtered.forEach((num, i) => {
+    nums[i] = num;
   });
 
   return filtered.length;
