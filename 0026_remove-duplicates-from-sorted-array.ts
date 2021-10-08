ad90abfc41c2d0e2f492da469380cb77fe70e20c
7696122 <7696122@gmail.com>
2021-10-08 09:28:47 +0900

    0026_remove-duplicates-from-sorted-array.ts

--- 0026_remove-duplicates-from-sorted-array.ts
+++ 0026_remove-duplicates-from-sorted-array.ts
@@ -1,9 +1,8 @@
 function removeDuplicates(nums: number[]): number {
-  const unionNumbers = nums.filter(
-    (item, index) => nums.indexOf(item) === index
-  );
-  unionNumbers.forEach((item, index, items) => {
-    nums[index] = items[index] ? items[index] : undefined;
-  });
+  nums
+    .filter((item, index) => nums.indexOf(item) === index)
+    .forEach((item, index, items) => {
+      nums[index] = items[index] ? items[index] : undefined;
+    });
   return unionNumbers.length;
 }
