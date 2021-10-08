4d5686102cddaee5a624e7ea62d16d79f633a474
7696122 <7696122@gmail.com>
2021-10-08 11:30:19 +0900

    0035_search-insert-position.ts

--- 0035_search-insert-position.ts
+++ 0035_search-insert-position.ts
@@ -1,6 +1,7 @@
 function searchInsert(nums: number[], target: number): number {
   let ret = nums.indexOf(target);
   if (ret > -1) return ret;
+
   nums.every((num, index) => {
     if (num < target) {
       ret = index + 1;
