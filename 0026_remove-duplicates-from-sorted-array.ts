deb4b87e798720dfd71c8f745f0c7871bc869f33
7696122 <7696122@gmail.com>
2021-10-08 09:27:47 +0900

    0026_remove-duplicates-from-sorted-array.ts

--- 0026_remove-duplicates-from-sorted-array.ts
+++ 0026_remove-duplicates-from-sorted-array.ts
@@ -1,7 +1,8 @@
 function removeDuplicates(nums: number[]): number {
-  nums
-    .filter((item, index) => nums.indexOf(item) === index)
-    .forEach((item, index, items) => {
-      nums[index] = items[index] ? items[index] : undefined;
-    });
+  const unionNumbers = nums.filter(
+    (item, index) => nums.indexOf(item) === index
+  );
+  unionNumbers.forEach((item, index, items) => {
+    nums[index] = items[index] ? items[index] : undefined;
+  });
 }
