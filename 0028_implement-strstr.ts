d6a9331590e0503bbddeea8fced4890fff048889
7696122 <7696122@gmail.com>
2021-10-08 11:00:00 +0900

    0028_implement-strstr.ts

--- 0028_implement-strstr.ts
+++ 0028_implement-strstr.ts
@@ -3,6 +3,6 @@ function strStr(haystack: string, needle: string): number {
   if (needle.length === 0) return 0;
   let point = 0;
   needle.forEach((ch, i) => {
-    haystack.indexOf;
+    haystack.indexOf(ch);
   });
 }
