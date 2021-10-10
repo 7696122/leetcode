8c929e1de715cbcaf28ea4eee21ee618a644ecb8
7696122 <7696122@gmail.com>
2021-10-11 06:25:58 +0900

    0053_maximum-subarray.ts

--- 0053_maximum-subarray.ts
+++ 0053_maximum-subarray.ts
@@ -13,20 +13,5 @@ function maxSubArray(nums: number[]): number {
       max_so_far = max_ending_here;
     }
   });
-  // for (let i = 0; i < nums.length; i++) {
-  //   // include current element to previous subarray only
-  //   // when it can add to a bigger number than itself.
-  //   if (arr[i] <= max_ending_here + arr[i]) {
-  //     max_ending_here += arr[i];
-  //   }
-
-  //   // Else start the max subarray from current element
-  //   else {
-  //     max_ending_here = arr[i];
-  //   }
-  //   if (max_ending_here > max_so_far) {
-  //     max_so_far = max_ending_here;
-  //   }
-  // }
   return max_so_far;
 }
