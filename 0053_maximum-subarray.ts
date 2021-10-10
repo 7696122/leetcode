60882b20d6fdd236703d16f357a04b5af05a5989
7696122 <7696122@gmail.com>
2021-10-10 09:53:45 +0900

    0053_maximum-subarray.ts

--- 0053_maximum-subarray.ts
+++ 0053_maximum-subarray.ts
@@ -1,7 +1,20 @@
 function maxSubArray(nums: number[]): number {
-  let sum = nums.reduce((item, acc) => (acc += item));
-  let temp = 0;
-  let i = 0;
-  let j = 0;
-  return;
+  let max_ending_here = 0,
+    max_so_far = Number.MIN_VALUE;
+  for (let i = 0; i < size; i++) {
+    // include current element to previous subarray only
+    // when it can add to a bigger number than itself.
+    if (arr[i] <= max_ending_here + arr[i]) {
+      max_ending_here += arr[i];
+    }
+
+    // Else start the max subarray from current element
+    else {
+      max_ending_here = arr[i];
+    }
+    if (max_ending_here > max_so_far) {
+      max_so_far = max_ending_here;
+    }
+  }
+  return max_so_far;
 }
