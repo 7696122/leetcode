bd99bd3df6da9fec8c38baea75a692c195be5b3f
7696122 <7696122@gmail.com>
2021-10-16 06:05:10 +0900

    main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-import {twoOutOfThree} from './2032_two-out-of-three'
+import { twoOutOfThree } from './2032_two-out-of-three'
 
-const nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+const nums1 = [1, 1, 3, 2], nums2 = [2, 3], nums3 = [3]
