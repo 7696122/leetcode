fc85a712fbd33b201ce8ab0d3bf9853cfe2968a5
7696122 <7696122@gmail.com>
2021-10-16 06:07:37 +0900

    main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ import { twoOutOfThree } from './2032_two-out-of-three';
 
 let nums1 = [1, 1, 3, 2], nums2 = [2, 3], nums3 = [3];
 
-twoOutOfThree(nums1, nums2, nums3)
+const res = twoOutOfThree(nums1, nums2, nums3)
+console.log(res)
