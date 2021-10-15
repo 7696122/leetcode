7acc448da7dc90804896ca8cd19e966052c976fa
7696122 <7696122@gmail.com>
2021-10-16 06:09:14 +0900

    2032_two-out-of-three.ts

--- 2032_two-out-of-three.ts
+++ 2032_two-out-of-three.ts
@@ -1,4 +1,9 @@
-export function twoOutOfThree(nums1: number[], nums2: number[], nums3: number[]): number[] {
-    const ret = [];
-    return ret;
-};
+export function twoOutOfThree(
+  nums1: number[],
+  nums2: number[],
+  nums3: number[]
+): number[] {
+  const ret = [];
+  console.log(nums1, nums2, nums3);
+  return ret;
+}
